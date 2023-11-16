5ee328c17d5c20000e608a9a2bcf00ab2f0e584f
Rafael Gomez <rgomez2014@gmail.com>
2023-11-15 22:10:19 -0600

    removed some comments

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,6 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    // get a single user by their id (assumes that if no user is passed, context has the user info)
     me: async (parent, args, context) => {
       if (context.user) {
         const foundUser = await User.findOne({ _id: context.user._id }).populate('savedBooks');
@@ -14,14 +13,12 @@ const resolvers = {
   },
 
   Mutation: {
-    // create a user, sign a token, and send it back
     addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
     },
 
-    // login a user, sign a token, and send it back
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
@@ -38,7 +35,6 @@ const resolvers = {
       return { token, user };
     },
 
-    // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
     saveBook: async (parent, { input }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
@@ -51,7 +47,6 @@ const resolvers = {
       throw new AuthenticationError('You need to be logged in!');
     },
 
-    // remove a book from `savedBooks`
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
