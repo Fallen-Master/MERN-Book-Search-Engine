dc5694902961467e538ee53b9a19c3f8a33628ee
Rafael Gomez <rgomez2014@gmail.com>
2023-11-15 22:05:37 -0600

    refactored index.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,18 +5,20 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    let token = req.headers.authorization || '';
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length).trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      // If there is no token, return the request object as is
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,15 +27,12 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
